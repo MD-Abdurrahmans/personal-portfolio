c8deed31f01d8ea17dd27347a9a6c582a107d976
Abdurrahman Raj <abraaz763@gmail.com>
2022-11-09 23:08:45 -0800

    hamber cheack

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 let hambar=document.querySelector(".hambar");
 
-let active=document.querySelector(".about-fixedContainer");
+let active=document.querySelector(".fixedIntro-container");
 hambar.addEventListener("click",function(){
      active.classList.toggle("active");
 
